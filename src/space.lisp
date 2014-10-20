5204907adeecd4cb3b1e7e1a74ab43d4c16e748d
Logan Mohseni <mohsenil85@gmail.com>
2014-10-20 08:02:31 -0700

    zoop

--- src/space.lisp
+++ src/space.lisp
@@ -30,6 +30,7 @@
     (setf *cells* (make-hash-table :size (* w h) :test 'equal))))
 
 (defun init ()
+  (kill-all)
   (set-w-and-h)
   (disable-echoing)
   (cl-charms/low-level:curs-set 0)
@@ -41,9 +42,18 @@
   (cl-charms/low-level:init-pair blue 
        cl-charms/low-level:color_blue charms/ll:color_black )
   (cl-charms/low-level:init-pair green 
-        cl-charms/low-level:color_green charms/ll:color_black )
-  ()
-  
+        cl-charms/low-level:color_green charms/ll:color_black ))
+
+(defparameter *dbg* (format nil "foo ~A" *player*))
+(defun move-left (p)
+  (write-string-at-point  *standard-window* *dbg* 5 5 )
+  )
+
+(defun move-right (p)
+  (move-left p))
+
+(defun testfun ()
+  (write-string-at-point *standard-window*)
   )
 
 (defun quit ()
@@ -52,7 +62,7 @@
 (defun get-input ()
  (let ((c (get-char *standard-window* :ignore-error t)))
   (case c
-    ((nil) nil)
+    ((#\t) (testfun))   
     ((#\l) (move-left *player*))   
     ((#\h) (move-right *player*))   
     ((#\q) (quit)))))
@@ -70,30 +80,50 @@
                          (car coords) 
                          (cdr coords))))
 
+(defstruct tile x y chr color)
+(defun create-tile (x y chr color)
+  (create-cell x y chr color))
 
 ;     A
 ;   <}V{>
-(defun create-player ()
-  (let ((orig (floor (/ *width* 2 ))))
-   (list (create-cell orig 0 #\V blue )) 
-   (create-cell (+ 1 orig) 0 #\V red ) 
-   (create-cell (- 1 orig) 0 #\V red ) ))
+(defun player-cells ()
+  (let ((x (floor (/ (- *width* 3) 2)))
+        (y (- *height* 2)))
+    (list (make-tile :x x :y y :chr #\V :color blue)
+          (make-tile :x x :y (- y 1) :chr #\A :color blue)
+          (make-tile :x (- x 1) :y y :chr #\} :color blue)
+          (make-tile :x (+ x 1) :y y :chr #\{ :color blue)
+          (make-tile :x (- x 2) :y y :chr #\< :color blue)
+          (make-tile :x (+ x 2) :y y :chr #\> :color blue))))
+
+(defun init-player ()
+  (let ((cls (player-cells)))
+    (setf *player* (make-player :health 100 :cells cls))
+    (loop for p in cls do
+          (create-cell (tile-x p) (tile-y p) (tile-chr p)  (tile-color p)))))
 
 
+;(- 10 1)
+
+;*cells*
+;(log-cells)
+;(kill-all)
 
 (defun update-world ()
   (create-cell 2 2 #\# red)
   (create-cell 2 3 #\$ blue)
   (create-cell 2 4 #\# red)
   )
+;*player*
 
-(update-world)
+;(update-world)
 (defun draw-world ()
   (maphash #'draw-cell *cells*))
 
 (defun main ()
   (with-curses  ()
     (init)
+    (init-player)
     (loop :named main-loop
           :while *running*
           :do
